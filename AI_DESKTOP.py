90ffb008f6d75f5339c6cb2e596d5138d2098f9c
Nitish Kumar Yadav <yadav27nitish@gmail.com>
2021-06-26 09:56:07 +0530

    Update AI_DESKTOP.py

--- AI_DESKTOP.py
+++ AI_DESKTOP.py
@@ -1,15 +1,18 @@
-import pyttsx3
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr  #pip install speechRecognition
 import datetime
-import speech_recognition as sr
-import wikipedia
+import wikipedia #pip install wikipedia
 import webbrowser
 import os
+import smtplib
+import pyaudio
 
 engine = pyttsx3.init('sapi5')
 # sapi5 is help to take voice from window. it take inbuilt voice from window which is provided by microsoft.
 voices = engine.getProperty('voices')
-
+# print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
+
 
 def speak(audio):
     # this function helps to speak the voice
@@ -17,53 +20,91 @@ def speak(audio):
     engine.runAndWait()
     # Without this command, speech will not be audible to us.
 
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
+    if hour>=0 and hour<12:
         speak("Good Morning!")
-    elif hour >= 12 and hour < 18:
+
+    elif hour>=12 and hour<18:
         speak("Good Afternoon!")
+
     else:
         speak("Good Evening!")
-    speak("Hey I am Sivi. How may i help you.")
 
+    speak("I am Shivi. Please tell me how may I help you")
 
-def takecommand():
-    # It takes microphone input from the user and returns string output
+def takeCommand():
+    #It takes microphone input from the user and returns string output
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1  # this line wait for listening till 1 sec in case someone is not speak
+        r.pause_threshold = 1    # this line wait for listening till 1 sec in case someone is not speak
         audio = r.listen(source)
+
     try:
-         # In case sivi is not able to understand.
+        # In case sivi is not able to understand.
         print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')  # Using google for voice recognition.
-        print(f"User said: {query}\n")  # User query will be printed.
+        query = r.recognize_google(audio, language='en-in')   # Using google for voice recognition.
+        print(f"User said: {query}\n")     # User query will be printed
 
     except Exception as e:
-        print("Say that again please...")  # Say that again will be printed in case of improper voice
-        return "None"  # None string will be returned
+        # print(e)
+        print("Say that again please...")   # Say that again will be printed in case of improper voice
+        return "None"   # None string will be returned
     return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
 
 if __name__ == "__main__":
     wishMe()
-    #while (True):
-    query = takecommand()
-    if 'Wikipedia' or 'wikipedia' in query:  # if wikipedia found in the query then this block will be executed
-        speak('Searching Wikipedia...')
-        query = query.replace("wikipedia", "")
-        results = wikipedia.summary(query, sentences=2)
-        speak("According to Wikipedia")
-        print(results)
-        speak(results)
-    elif 'open Youtube' or 'youtube' or 'YouTube' in query:
-        webbrowser.open("youtube.com")
-    elif 'Google' or 'google' in query:
-        webbrowser.open("google.com")
-    elif 'Play music' or 'play music' or 'Play Music' in query:
-        webbrowser.open("youtube.com")
-    elif 'Time' or 'time' in query:
-        c_time = datetime.datetime.now()
-        speak(f"Time is {c_time}")
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+
+        elif 'play music' or 'open music' in query:
+            webbrowser.open("https://www.youtube.com/watch?v=AzOvsmpQVKM")
+            
+            
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+
+        elif 'email to nitish' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "yadav27nitish@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry. I am not able to send this email")
